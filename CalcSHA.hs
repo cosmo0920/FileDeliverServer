624e983aa3e1936409c6f025840aca6e7455b91c
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-11-14 10:19:46 +0900

    more practical json generate

--- CalcSHA.hs
+++ CalcSHA.hs
@@ -3,27 +3,27 @@ import Data.Digest.Pure.SHA
 import System.IO.Unsafe
 import qualified Data.ByteString.Lazy.Char8 as BL
 
-sha1Digest :: BL.ByteString -> String
-sha1Digest = showDigest . sha1
+shaDigest :: BL.ByteString -> String
+shaDigest = showDigest . sha1
 
-showSHA1 :: FilePath -> IO String
-showSHA1 dir = do
+showSHA :: FilePath -> IO String
+showSHA dir = do
   content <- BL.readFile dir
-  let digest = sha1Digest content
+  let digest = shaDigest content
   return digest
 
-showSHA1' :: FilePath -> String
-showSHA1' dir = unsafePerformIO $ do
+unsafeShowSHA :: FilePath -> String
+unsafeShowSHA dir = unsafePerformIO $ do
   content <- BL.readFile dir
-  let digest = sha1Digest content
+  let digest = shaDigest content
   return digest
 
-showSHA1WithSep' :: FilePath -> String
-showSHA1WithSep' dir = unsafePerformIO $ do
+unsafeShowSHAWithSep :: FilePath -> String
+unsafeShowSHAWithSep dir = unsafePerformIO $ do
   content <- BL.readFile dir
-  let digest = sha1Digest content
+  let digest = shaDigest content
   return $ "," ++ digest
 
 calcSHA :: [FilePath] -> [String]
 calcSHA dir = do
-  map showSHA1WithSep' dir
+  map unsafeShowSHAWithSep dir
--- MkJson.hs
+++ MkJson.hs
@@ -9,25 +9,17 @@ import Data.Text (Text)
 import qualified Data.Attoparsec as AP (Result(..),parseOnly)
 import qualified Data.Attoparsec.Number as N (Number(I,D))
 import qualified Data.ByteString.Lazy.Char8 as LC (unpack)
-import qualified Data.Map as M (lookup,delete)
-import GHC.Generics
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+import qualified Filesystem.Path.CurrentOS as FP
 import CalcSHA
 
 s :: String -> String
 s = id
 
-data AssetList = AssetList {
-                   filepath :: !FilePath
-                 , digest   :: !String
-                 } deriving (Show, Generic)
-
-instance FromJSON AssetList
-instance ToJSON AssetList
-
-assetList :: FilePath -> AssetList
+assetList :: FilePath -> Value
 assetList path = do
-  AssetList { filepath = path
-            , digest = showSHA1' path }
+  object [ T.pack path .= (unsafeShowSHA path) ]
 
 value :: [FilePath] -> Value
 value path = do
@@ -36,3 +28,22 @@ value path = do
 
 showValue :: [FilePath] -> String
 showValue = LC.unpack . encode . value
+
+remove :: Text -> Value -> Parser Value
+remove key val = Object . HM.delete key <$> parseJSON val
+
+-- modify :: a ->  v -> HM.HashMap a v -> HM.HashMap a v
+modify key val =  HM.adjust key <$> parseJSON val
+
+add key val = HM.insert key <$> parseJSON val
+
+forceResult :: Result t -> t
+forceResult (Success v) = v
+
+dropValue :: FP.FilePath -> Value -> Value
+dropValue dir fileValue = do
+  forceResult $ parse (remove (T.pack $ FP.encodeString dir)) fileValue
+
+-- modifyValue :: FP.FilePath -> Value -> Value
+-- modifyValue dir fileValue = do
+--   forceResult $ parse (modify (T.pack $ FP.encodeString dir)) fileValue
