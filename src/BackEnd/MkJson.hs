da479e61afbdb226bd1ca14ee0595a1f4b718daf
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-11-16 03:14:28 +0900

    remove unused import

--- src/BackEnd/MkJson.hs
+++ src/BackEnd/MkJson.hs
@@ -1,21 +1,8 @@
 module BackEnd.MkJson where
-import Control.Applicative (Applicative,pure,(<$>),(<*>))
-import Control.Monad (mzero,(=<<),(<=<),(>=>))
 import Data.Aeson
-import Data.Aeson.Types
-import Data.Either.Utils (forceEither)
-import Data.Maybe (catMaybes)
-import Data.Text (Text)
-import qualified Data.Attoparsec as AP (Result(..),parseOnly)
-import qualified Data.Attoparsec.Number as N (Number(I,D))
 import qualified Data.ByteString.Lazy.Char8 as LC (unpack)
-import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
-import qualified Filesystem.Path.CurrentOS as FP
 import BackEnd.CalcSHA
-
-s :: String -> String
-s = id
 
 assetList :: FilePath -> Value
 assetList path = do
@@ -27,22 +14,3 @@ value path = do
 
 showValue :: [FilePath] -> String
 showValue = LC.unpack . encode . value
-
-remove :: Text -> Value -> Parser Value
-remove key val = Object . HM.delete key <$> parseJSON val
-
--- modify :: a ->  v -> HM.HashMap a v -> HM.HashMap a v
-modify key val =  HM.adjust key <$> parseJSON val
-
-add key val = HM.insert key <$> parseJSON val
-
-forceResult :: Result t -> t
-forceResult (Success v) = v
-
-dropValue :: FP.FilePath -> Value -> Value
-dropValue dir fileValue = do
-  forceResult $ parse (remove (T.pack $ FP.encodeString dir)) fileValue
-
--- modifyValue :: FP.FilePath -> Value -> Value
--- modifyValue dir fileValue = do
---   forceResult $ parse (modify (T.pack $ FP.encodeString dir)) fileValue
